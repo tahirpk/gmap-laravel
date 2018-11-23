baf2d393433313103fac9d2fc9e2f27304c482f4
tahirpk <tahirpk@gmail.com>
2018-11-23 14:05:05 +0400

    link updated for view address

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,11 +53,11 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                | <a class="dropdown-item" href="{{ url('addresses') }}">
+                                 
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('addresses') }}">
                                       Add Address
                                     </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
