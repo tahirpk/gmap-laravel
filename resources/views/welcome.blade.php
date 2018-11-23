621f509342aa96bae712f1912bcdb9f037de42ba
tahirpk <tahirpk@gmail.com>
2018-11-23 14:08:09 +0400

    link updated for view address

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,8 +42,7 @@
         <div>
             <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/gmap') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/') }}">View Addresses
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -61,6 +60,9 @@
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('addresses') }}">Add Address</a>
                             </li>
                             <li class="nav-item">
                                 @if (Route::has('register'))
